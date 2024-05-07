d7d260f51ed57d2636e0ce7e650802a196f46037
Larissa L. Oliveira <worklarissaoliveira@gmail.com>
2024-05-07 12:25:52 -0300

    Organizando arquivos

--- Projeto-ViagemPraia/data-keys.py
+++ Projeto-ViagemPraia/data-keys.py
@@ -1,1 +1,2 @@
-#key_clima =
+key_clima = '2c29ccffc7528a922d96bc37334167a7'
+key_transito = 'v_MflLCMYc24J3LYOTalid2rTlHoZfKtoxTz0Bc8HQs'
---
+++ Projeto-ViagemPraia/soups/soupbalneabilidade.py
@@ -0,0 +1,33 @@
+#!pip install unidecode
+#!pip install beautifulsoup4
+
+import requests
+from bs4 import BeautifulSoup
+import warnings
+
+
+# URL da página que você deseja consumir
+url = 'https://praialimpa.net/sao-paulo/'
+
+#Lista de praias para buscar
+praias = ["Ubatuba, Caraguatatuba", "São Sebastião", "Ilhabela", "Bertioga", "Guarujá", "Santos", "São Vicente", "Praia Grande", "Mongaguá", "Itanhaéma", "Peruíbe", "Ilha Comprida", "Cubatão"]
+
+#Para ignorar o aviso de warnings
+warnings.filterwarnings("ignore", message="Unverified HTTPS request")
+
+response = requests.get(url, verify=False)
+response.encoding = 'UTF-8'
+
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, 'html.parser')
+    paragraphs = soup.find_all('section')
+    for p in paragraphs:
+        texto_formatado = p.text.replace('Própria', 'Própria - ').replace('Imprópria', 'Imprópria - ')
+        for praia in praias:
+            if praia in texto_formatado:
+                print(texto_formatado)
+                break
+
+else:
+    print(f"Erro ao acessar a página: {response.status_code}")
+
