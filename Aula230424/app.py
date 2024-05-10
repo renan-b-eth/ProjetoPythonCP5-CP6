f8e18088a176fd5d99915a359bf2fe29b7388564
Larissa L. Oliveira <worklarissaoliveira@gmail.com>
2024-05-09 21:13:11 -0300

    Adicionando exemplos

---
+++ Aula230424/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+ 
+
+
+if __name__ == '__main__':
+    app.run()
