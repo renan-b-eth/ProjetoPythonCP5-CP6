adf6ae9e41e612cc7223819ee701d80993bb78d3
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-10 10:20:44 -0300

    Iniciando implantação das atividades

--- Projeto-ViagemPraia/main.py
+++ Projeto-ViagemPraia/main.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    cidade = request.form['cidade']
+    email = request.form['email']
     return render_template('index.html')
 
 if __name__ == '__main__':
